3816dac8533184e546935d72f6aa4177c4e0f0d2
Naga Srinivas <nagasai.srinu@gmail.com>
2025-03-25 14:13:36 +0530

    Testing env variable

--- src/config/pinecone.js
+++ src/config/pinecone.js
@@ -2,6 +2,7 @@ import { Pinecone } from "@pinecone-database/pinecone";
 import { config } from 'dotenv';
 config();
 
+console.log("PUBLIC_DOMAIN", process.env.RAILWAY_PUBLIC_DOMAIN);
 console.log("Pinecone API Key", process.env.PINECONE_API_KEY && process.env.PINECONE_API_KEY.substring(0, 5));
 
 const pinecone = new Pinecone({
